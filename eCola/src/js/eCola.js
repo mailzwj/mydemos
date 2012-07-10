86c886ad7fefff45ccc7fc392009d8a07ed6e2da
wb-zhengwujiang.z <wb-zhengwujiang.z@taobao.com>
2012-07-10 16:17:09 +0800

    周视图数据按天呈现

--- eCola/src/js/eCola.js
+++ eCola/src/js/eCola.js
@@ -36,6 +36,7 @@ $(function(){
 
         wm.html(col_span + col_div);
 
+        wm.scrollTop(18 * ($(".week-main:eq(0) .time").eq(0).height() + 1));
         callback && callback();
     }
 
@@ -53,6 +54,24 @@ $(function(){
         callback && callback();
     }
 
+    function getStartAndEnd(srow, erow){
+        var val = {};
+        var stime = srow * 0.5 * 60,
+            etime = (erow + 1) * 0.5 * 60;
+        val.d = erow - srow;
+        val.startH = Math.floor(stime / 60);
+        val.startH = val.startH < 10 ? '0' + val.startH : val.startH;
+        val.startM = stime % 60;
+        val.startM = val.startM < 10 ? '0' + val.startM : val.startM;
+        val.endH = Math.floor(etime / 60);
+        val.endH = val.endH < 10 ? '0' + val.endH : val.endH;
+        val.endM = etime % 60;
+        val.endM = val.endM < 10 ? '0' + val.endM : val.endM;
+        val.start = [val.startH,val.startM].join(":");
+        val.end = [val.endH,val.endM].join(":");
+        return val;
+    }
+
     function initLayer(){
         var wm = $(".week-main").eq(0);
         var weekdays = $(".week-main:eq(0)>.col");
@@ -61,6 +80,7 @@ $(function(){
         var index_clicked = '';
         var html = '';
         var index = '';
+        var text = "";
         weekdays.each(function(i, eles){
             $(eles).mousedown(function(e){
                 var evt = e.target,
@@ -69,15 +89,20 @@ $(function(){
                 index = i;
                 clicked = true;
                 index_clicked = $.inArray(evt, hours);
-                var width = $(evt).width() + 1,
+                if(html != ''){
+                    $(html).remove();
+                }
+                var s2e = getStartAndEnd(index_clicked, index_clicked),
+                    width = $(evt).width() + 1,
                     height = $(evt).height(),
                     left = (index_par * width + fcol.width() + diff.x) + 'px',
                     top = (index_clicked * (height + 1) + diff.y ) + "px";
-                html = $('<div class="uplayer"></div>');
+                html = $('<div class="uplayer text-overflow"></div>');
                 html.css("left", left)
                     .css("top", top)
                     .css("width", width - diff.x + "px")
-                    .css("height", height + "px");
+                    .css("height", height + "px")
+                    .html("<h5>" + [s2e.start,s2e.end].join(" ~ ") + "</h5><div title='" + text + "'>" + text + "</div>");
                 wm.append(html);
             }).mousemove(function(e){
                 if(clicked && index == $.inArray(eles, weekdays)){
@@ -87,16 +112,21 @@ $(function(){
                         cha = index_sub - index_clicked;
                     var oh = $(html).height(),
                         nh = oh,
+                        et = index_sub,
                         top = parseInt(html.css("top"));
                     if(cha < 0){
                         top += cha * ($(evt).height() + 1);
                         nh = oh + Math.abs(cha) * ($(evt).height() + 1);
                         index_clicked = index_sub;
+                        et = Math.floor(oh / ($(evt).height() + 1)) + index_clicked + 1;
                     }else if(cha > 0){
                         nh = (cha + 1) * ($(evt).height() + 1);
+                        et = index_sub;
                     }
+                    var s2e = getStartAndEnd(index_clicked, et);
                     html.css("top", top + "px")
                         .css("height", nh + "px");
+                    $(html).children("h5").html([s2e.start,s2e.end].join(" ~ "))
                 }
                 $(document).mouseup(function(){
                     clicked = false;
@@ -106,11 +136,63 @@ $(function(){
         });
     }
 
+    function pushDataToCol(index, arr, size){
+        //console.log(index);
+        var main = $(".week-main:eq(0)");
+        var tc = $(".week-main:eq(0)>.col-time").eq(0);
+        var fc = $('<div class="uplayer fullalpha"></div>');
+        var diff = {"x":3,"y":0};
+        var m30 = 30 * 60 * 1000;
+        var pl = tc.width() + index * parseInt(size.w) + diff.x;
+        var pt = 0;
+        var st = 0, et = 0, h = 0;
+        $(arr).each(function(num, obj){
+            var as = obj.start.split(":");
+            var ae = obj.end.split(":");
+            st = (as[0] * 60 * 60 + as[1] * 60) * 1000;
+            et = (ae[0] * 60 * 60 + ae[1] * 60) * 1000;
+            h = Math.abs(et / m30 - st / m30) * size.h;
+            pt = (st / m30) * size.h;
+            var node = fc.clone();
+            $(node).css("left", pl + "px")
+                .css("top", pt + "px")
+                .css("width", (size.w - diff.x) + "px")
+                .css("height", h + "px")
+                .html("<h5>" + obj.start + " ~ " + obj.end + "</h5><div title='" + obj.content + "'>" + obj.content + "</div>");
+            main.append($(node));
+        });
+    }
+
+    function loadData(s){   //周startDate
+        var weekdays = $(".week-main:eq(0)>.col");
+        $.getScript("ajax/data.json", function(){
+            //console.log(json);
+            $(json).each(function(i, val){
+                weekdays.each(function(j, node){
+                    if($(node).attr("date") == new Date(val.date).getTime()){
+                        var size = {"w":$(node).width(), "h":($(node).children(".time:eq(0)").height() + 1)}
+                        pushDataToCol(j, val.log, size);  //将数据添加入对应列表
+                    }
+                });
+            });
+        });
+    }
+
     var nd = new Date();
     var tw = getThisWeek(nd.getFullYear(), nd.getMonth() + 1, nd.getDate());
     var cws = tw.weekStart;
-    updateWeekHeader(cws);
-    createWeekTable(cws, initLayer);
+    var g_hash = cws.replace(/\//g, "-"); //全局变量，存储当前hash值
+    var reg = /#(\d{4}-\d{2}-\d{2})/g
+    if(reg.test(window.location.href)){
+        g_hash = RegExp.$1;
+        RegExp.lastIndex = 0;
+    }else{
+        window.location.hash = "#" + g_hash;
+    }
+    $(".week-main").bind("selectstart",function(){return false;});
+    updateWeekHeader(g_hash.replace(/-/g, "/"));
+    createWeekTable(g_hash.replace(/-/g, "/"), initLayer);
+    loadData(g_hash.replace(/-/g, "/"));
 
     //console.log(getPreviousWeek(nd.getFullYear(), nd.getMonth() + 1, nd.getDate()));
 });
