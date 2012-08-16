6a893e38a6e07d20a8f003ef0a76463353248664
wb-zhengwujiang.z <wb-zhengwujiang.z@taobao.com>
2012-08-16 14:19:10 +0800

    reindex.js

--- eCola/src/js/reindex.js
+++ eCola/src/js/reindex.js
@@ -7,7 +7,7 @@
 
 KISSY.use("sizzle",function(S){
 	var D = S.DOM, E = S.Event;
-	var html = '';
+	var html = '', rowHeight = 26;
 	function createWeekTable(fd, callback){
         var wm = D.get(".week-main"),
             col_span = '<div class="col-time time-line">',
@@ -85,10 +85,10 @@ KISSY.use("sizzle",function(S){
 
 	function setNowTimeLine(){
         var line = D.get("#nowTimeLine");
-        var line_p = D.get(".week-main:eq(0)");
-        var h_p = D.height(D.children(".time-line", line_p)[0]);
-        var line_w = D.width(D.children(".col", line_p)[0]);
-        var line_left = D.width(D.get(".week-main:eq(0) .time-line"));
+        var line_p = S.one(".week-main");
+        var h_p = D.height(D.children(line_p, ".time-line")[0]);
+        var line_w = D.width(D.children(line_p, ".col")[0]);
+        var line_left = D.width(S.one(".week-main:eq(0) .time-line"));
         var timecount = 24 * 60 * 60 * 1000;
         var nowcount = new Date();
         var day = nowcount.getDay() == 0 ? 7 : nowcount.getDay();
@@ -96,7 +96,7 @@ KISSY.use("sizzle",function(S){
         var percent = (nowcount.getTime() - daycount.getTime()) / timecount;
         var line_top = Math.floor(h_p * percent) - 1;
         day -= 1;
-        line_left += day * D.outerWidth(D.children(".col", line_p)[0]);
+        line_left += day * D.outerWidth(D.children(line_p, ".col")[0]);
         D.css(line, "display", "block");
         D.css(line, "left", line_left);
         D.css(line, "top", line_top);
@@ -104,6 +104,260 @@ KISSY.use("sizzle",function(S){
         setTimeout(function(){setNowTimeLine();}, 60000);
     }
 
+    function getStartAndEnd(srow, h){
+        var val = {};
+        var stime = srow * 0.5 * 60,
+            erow = srow + Math.ceil(h / rowHeight),
+            etime = erow * 0.5 * 60;
+        val.d = erow - srow;
+        val.startH = Math.floor(stime / 60);
+        val.startH = val.startH < 10 ? '0' + val.startH : val.startH;
+        val.startM = stime % 60;
+        val.startM = val.startM < 10 ? '0' + val.startM : val.startM;
+        val.endH = Math.floor(etime / 60);
+        val.endH = val.endH < 10 ? '0' + val.endH : val.endH;
+        val.endM = etime % 60;
+        val.endM = val.endM < 10 ? '0' + val.endM : val.endM;
+        val.start = [val.startH,val.startM].join(":");
+        val.end = [val.endH,val.endM].join(":");
+        return val;
+    }
+
+    function changeFormAction(){
+        var form = S.one("#form-layer"),
+            nodes = S.all(".week-main:eq(0) .fullalpha");
+        nodes.click(function(){
+            if(S.all(".week-main:eq(0) .text-empty").length > 0){
+                D.remove(S.all(".week-main:eq(0) .text-empty"));
+            }
+            rePositionForm(this, "update");
+        });
+    }
+
+    function rePositionForm(dom, mth){
+        var f = S.one("#form-layer"),
+            w = S.one(".week-main"),
+            node = S.one(dom),
+            dl = parseInt(D.css(node, "left")),
+            dt = parseInt(D.css(node, "top")),
+            dw = D.outerWidth(node),
+            dh = D.outerHeight(node),
+            fw = D.outerWidth(f),
+            fh = D.outerHeight(f),
+            fl = dl + dw + 20,
+            ft = dt + (dh - fh) / 2 - D.scrollTop(w),
+            cat = D.attr(node, "cat");
+        D.removeClass(f, "outOfTheWindow");
+        if (fl + fw > D.width(window)){
+            fl -= (fw + dw + 40);
+            D.addClass(f, "outOfTheWindow");
+        }
+        D.css(f, "left", fl);
+        D.css(f, "top",ft);
+        D.css(f, "display", "block");
+        if(cat == "" || cat == "null"){cat = 0;}
+        D.val(D.query("textarea", f)[0], D.html(D.children(node, "div")[0]));
+        D.query("textarea", f)[0].focus();
+        D.val(D.query("input[type='hidden']")[0], D.attr(node, "id"));
+        D.val(D.query("input[type='hidden']")[1], D.attr(node, "date-start"));
+        D.val(D.query("input[type='hidden']")[2], D.attr(node, "date-end"));
+        D.attr(S.all("#ecola-cat option[value='" + cat + "']"), "selected", true);
+        D.attr(S.one(".ecola-del"), "href", "worklogs/" + D.attr(node, "id") + ".json");
+        if(mth == "add"){
+            D.attr(S.one(".ecola-save"), "href", "worklogs.json");//新增日志入口
+            D.css(S.one(".ecola-del"), "visibility", "hidden");
+        }else if(mth == "update"){
+            D.attr(S.one(".ecola-save"), "href", "worklogs/" + D.attr(node, "id") + ".json");//修改日志入口
+            D.css(S.one(".ecola-del"), "visibility", "visible");
+        }
+
+        E.detach(S.one(".ecola-save"), "click");
+        E.detach(S.one(".ecola-del"), "click");
+        E.on(S.one(".ecola-save"), "click", function(){
+            var url = D.attr(this, "href");
+            var param = {}, postData = {};
+            param.content = D.val(S.one("textarea"));
+            param.id = D.val(D.query("input[type='hidden']")[0]);
+            param.begin_at = D.val(D.query("input[type='hidden']")[1]);
+            param.end_at = D.val(D.query("input[type='hidden']")[2]);
+            param.logtype_id = parseInt(D.val(S.one("#ecola-cat")));
+            postData.worklog = param;
+            postData.authenticity_token = D.attr(S.one('meta[name="csrf-token"]'), "content");
+            if(mth == "update"){
+                postData._method = "put";
+            }
+            S.IO({
+                type: "POST",
+                url: url,
+                data: postData,
+                success: function(data){
+                    var json = data;
+                    if(json.status == "success"){
+                        D.html(D.children(node, "div")[0], param.content);
+                        D.attr(node, "id", json.id);
+                        D.attr(node, "cat", json.cat);
+                        D.removeClass(node, "text-empty");
+                        D.addClass(node, "fullalpha");
+                        D.css(S.one("#form-layer"), "display", "none");
+                        html = '';
+                        changeFormAction();
+                    }else{
+                        alert("日志保存失败了，休息一会儿再来吧！");
+                    }
+                }
+            });
+            return false;
+        });
+
+        E.on(S.one(".ecola-del"), "click", function(){
+            var url = D.attr(this, "href");
+            var flag = confirm("日志一经删除无法恢复，确定删除该条日志吗？");
+            if(flag){
+                S.IO.post(url, {"_method":"delete"}, function(data){
+                    var json = data;
+                    if(json.status == "success"){
+                        D.css(S.one("#form-layer"), "display", "none");
+                        //alert("日志删除成功");
+                        D.remove(node);
+                    }else{
+                        alert("删除失败了，请稍后重试");
+                    }
+                });
+            }
+
+            return false;
+        });
+        //console.log(fw);
+    }
+
+    function initLayer(){
+        var wm = S.one(".week-main");
+        var weekdays = S.all(".week-main:eq(0)>.col");
+        var fcol = S.one(".week-main:eq(0)>.col-time"), diff = {"x":1,"y":0};
+        var clicked = false;
+        var index_clicked = '';
+        var index = '';
+        var text = "";
+        var m_start = {x: 0, y: 0};//鼠标点击位置
+        var m_row = {x: 0, y: 0};//鼠标过渡位置
+        var m_end = {x: 0, y: 0};//鼠标移动位置
+        E.on(weekdays, "mousedown", function(e){
+            index = S.indexOf(this, weekdays);
+            m_start.x = e.clientX;
+            m_start.y = e.clientY;
+            m_row.x = e.clientX;
+            m_row.y = e.clientY;
+            var evt = e.target,
+                hours = D.children(this, ".time"),
+                index_par = S.indexOf(this, weekdays);//列索引
+            clicked = true;
+            index_clicked = S.indexOf(evt, hours);//起始行索引
+            if(html != ''){
+                D.remove(html);
+                D.css(S.one("#form-layer"), "display", "none");
+            }
+            var width = D.outerWidth(evt),
+                height = rowHeight,
+                left = (index_par * width + D.width(fcol) + diff.x) + 'px',
+                top = (index_clicked * height + diff.y ) + "px";
+            html = D.create('<div class="uplayer text-empty"></div>');
+            D.attr(html, "id", "");
+            D.attr(html, "cat", "");
+            D.css(html, "left", left);
+            D.css(html, "top", top);
+            D.css(html, "width", width - diff.x - 2);
+            D.css(html, "height", 0);
+            D.append(html, wm);
+            return false;
+        });
+        E.on(wm, "mousemove", function(e){
+            if(clicked){
+                m_end.x = e.clientX;
+                m_end.y = e.clientY;
+                if(m_end.y - m_row.y >= rowHeight / 2){
+                    m_row.y += rowHeight;
+                    var h_height = D.height(html);
+                    h_height = h_height > 0 ? h_height : -2;
+                    D.css(html, "height", h_height + rowHeight);
+                }else if(m_end.y - m_row.y <= -(rowHeight / 2)){
+                    m_row.y -= rowHeight;
+                    var h_height = D.height(html);
+                    if(m_row.y <= m_start.y){
+                        m_row.y = m_start.y;
+                        D.css(html, "height", 0);
+                    }else{
+                        D.css(html, "height", h_height - rowHeight);
+                    }
+                }
+                var hH = D.height(html) + 2;
+                if(hH > 2){
+                    var s2e = getStartAndEnd(index_clicked, hH);
+                    var date = new Date(parseInt(D.attr(weekdays[index], "date")));
+                    var datestart = date.getFullYear() + "-" + addZero(date.getMonth() + 1) + "-" + addZero(date.getDate()) + " " + s2e.start + ":00";
+                    var dateend = date.getFullYear() + "-" + addZero(date.getMonth() + 1) + "-" + addZero(date.getDate()) + " " + s2e.end + ":00";
+                    D.attr(html, "date-start", datestart);
+                    D.attr(html, "date-end", dateend);
+                    D.html(html, "<h5>" + [s2e.start,s2e.end].join(" ~ ") + "</h5><div title='" + text + "'>" + text + "</div>");
+                }
+            }
+            return false;
+        });
+        E.on(document, "mouseup", function(){
+            if(clicked){
+                rePositionForm(html, "add");
+            }
+            clicked = false;
+            return false;
+        });
+    }
+
+    function pushDataToCol(index, val, size, callback){
+        var main = S.one(".week-main");
+        var tc = S.one(".week-main:eq(0)>.col-time");
+        var fc = D.create('<div class="uplayer fullalpha"></div>');
+        var diff = {"x":1,"y":0};
+        var m30 = 30 * 60 * 1000;
+        var pl = D.width(tc) + index * parseInt(size.w) + diff.x;
+        var pt = 0;
+        var st = 0, et = 0, h = 0;
+        var as = val.start.split(":");
+        var ae = val.end.split(":");
+        st = (as[0] * 60 * 60 + as[1] * 60) * 1000;
+        et = (ae[0] * 60 * 60 + ae[1] * 60) * 1000;
+        h = Math.abs(et / m30 - st / m30) * size.h;
+        pt = (st / m30) * size.h;
+        var node = fc.clone();
+        D.attr(node,"id", val.id)
+        D.attr(node, "cat", (val.cat && val.cat != null) ? val.cat : 0)
+        D.attr(node, "date-start", val.date.replace(/\//g, "-") + " " + val.start + ":00")
+        D.attr(node, "date-end", val.date.replace(/\//g, "-") + " " + val.end + ":00")
+        D.css(node, "left", pl)
+        D.css(node, "top", pt)
+        D.css(node, "width", size.w - diff.x - 2)
+        D.css(node, "height", h - 2)
+        D.html(node, "<h5>" + val.start + " ~ " + val.end + "</h5><div title='" + val.content + "'>" + val.content + "</div>");
+        D.append(node, main);
+
+        callback && callback();
+    }
+
+    function loadData(s){   //周startDate
+        var weekdays = S.all(".week-main:eq(0)>.col");
+        S.getScript("worklogs.json?s=" + new Date(s.replace(/-/g, "/")).getTime(), function(){
+            S.each(json, function(val){
+                S.each(weekdays, function(node){
+                    var j = S.indexOf(node, weekdays);
+                    if(D.attr(node, "date") == new Date(val.date).getTime()){
+                        var size = {"w":D.width(node), "h": rowHeight};
+                        pushDataToCol(j, val, size, function(){
+                            D.css(S.one("#loading"), "display", "none");
+                            changeFormAction();
+                        });  //将数据添加入对应列表
+                    }
+                });
+            });
+        });
+    }
 
 	var nd = new Date();
     var tw = getThisWeek(nd.getFullYear(), nd.getMonth() + 1, nd.getDate());
@@ -133,7 +387,7 @@ KISSY.use("sizzle",function(S){
     D.css(D.get("#loading"), "display", "block");
     updateWeekHeader(g_hash.replace(/-/g, "/"));
     createWeekTable(g_hash.replace(/-/g, "/"), function(){
-        //initLayer();
+        initLayer();
         if(includeToday(g_hash)){
             setNowTimeLine();
             addToday();
@@ -143,7 +397,7 @@ KISSY.use("sizzle",function(S){
             D.attr(today, "disabled", false);
         }
     });
-    //loadData(g_hash);
+    loadData(g_hash);
 
 	S.all(window).on("hashchange", function(){
         var hs = window.location.href;
@@ -165,7 +419,7 @@ KISSY.use("sizzle",function(S){
                         D.attr(today, "disabled", false);
                     }
                 });
-                //loadData(g_hash);
+                loadData(g_hash);
             }
         }else{
             g_hash = cws.replace(/\//g, "-");
@@ -189,7 +443,7 @@ KISSY.use("sizzle",function(S){
         var updateflag = false;
         var ue = D.get(evt);
         while(ue.tagName != "BODY"){
-            if(D.hasClass(ue, "fullalpha")){
+            if(D.hasClass(ue, "fullalpha") || D.hasClass(ue, "text-empty")){
                 updateflag = true;
                 break;
             }
