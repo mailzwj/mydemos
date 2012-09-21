0696db53719b0f7d0f20b2d5aa5a6d251fc60286
Mr.Zheng <mailzwj@126.com>
2012-09-21 15:01:31 +0800

    新增NodeJS测试项目nodejs

---
+++ nodejs/index.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var ev = require("events");
+
+http.createServer(function(req, res){
+	res.write("<!DOCTYPE HTML>\n\n");
+	res.write("<html>\n");
+	res.write("<head>\n");
+	res.write("<meta charset=\"UTF-8\">\n");
+	res.write("<title>NodeJS 入门</title>\n");
+	res.write("</head>\n");
+	res.write("<body>\n");
+	res.write("Hello Word!");
+	res.write("\n</body>\n");
+	res.end("</html>");
+	console.log(req.headers.host);
+}).listen(8088);
+
+/*document.on("click", function(){
+	http.get({host: "127.0.0.1", port: 8088, path: "server.html"}, function(response){
+		document.getElementsByTagName("body")[0].innerHTML = response.content;
+	}).on("error", function(e){
+		console.log("Got Error:" + e.message);
+	});
+});*/
+
+console.log("Server is Running at http://127.0.0.1:8088/");
