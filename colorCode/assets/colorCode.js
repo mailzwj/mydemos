8ad5c3b9becc22cf46ec850f9b8bf0ff22abdec7
wb-zhengwujiang.z <wb-zhengwujiang.z@taobao.com>
2012-11-29 20:45:13 +0800

    修改js正则

--- colorCode/assets/colorCode.js
+++ colorCode/assets/colorCode.js
@@ -1,39 +1,41 @@
 (function(){
-	function CC(){};
+	"use strict";
+	function CC(){}
 	window.CC = CC;
 	//创建默认配置，全局，可供外部修改
 	CC.cfg = {
-		codeBox: "pre.colorCode",
+		codeBox: "pre.colorCode"
 	};
 	CC.regs = {
 		html: {
 			blank: /(^\s+)|(\s+$)/g,
 			tag: /(\&lt;\/?)([a-zA-Z]+)/g,
-			prop: /\s+([a-zA-Z]+)="(<|")/g,
-			propval: /="(\S+)"/g,
+			prop: /\s+([a-zA-Z]+)\="(<|")/g,
+			propval: /\="(\S+)"/g,
 			notestart: /\&lt;\!--/g,
 			noteend: /--\&gt;/g
 		},
 		css: {
-			style: /(^|\n)([a-zA-Z0-9-_\.#\s,]+)(?={)/g,
+			style: /(^|\n)([\w\-_\.#\s,]+)(?=\{)/g,
 			tag: /(^|\n|\s+)([a-zA-Z]+)/g,
-			clsorid: /([\.#][a-zA-Z0-9-_]+)/g,
-			prop: /(^|\n|\s+|{|;)([a-zA-Z-]+)(?=\s?:)/g,
-			propval: /(:\s?)([a-zA-Z0-9-_:\/#@\.]+)(;?)/g,
+			clsorid: /([\.#][\w\-]+)/g,
+			prop: /(^|\n|\s+|\{|;)([a-zA-Z\-]+)(?=\s?:)/g,
+			propval: /(:\s?)([\w\-:\/#@\.]+)(;?)/g,
 			notestart: /\/\*/g,
 			noteend: /\*\//g
 		},
 		js: {
-			keyword: /(new|do|break|while|typeof|delete|for|continue|if|with|try|switch|in|instanceof|case|default|throw|catch|finally|else|return)(?=;|\s|}|\(|{|:)/g,
+			keyword: /(new|do|break|while|typeof|delete|for|continue|if|with|try|switch|in|instanceof|case|default|throw|catch|finally|else|return)(?=;|\s|\(|\{|\}|\:)/g,
 			number: /(\d+|false|true|null)/g,
 			notestart: /\/\*/g,
-			noteend: /\*\//g
+			noteend: /\*\//g,
+			noteline: /(\/\/.+)(?=\n)/g
 		}
-	}
+	};
 	CC.colorCode = function(){
 		this.cs = CC.cfg;
 		this._init();
-	}
+	};
 	CC.colorCode.prototype = {
 		_hasClass: function(node, cls){
 			var reg = new RegExp("(^|\\s+)" + cls + "(\\s+|$)", "g");
@@ -61,7 +63,6 @@
 			}
 		},
 		_formart: function(txt){
-			var _this = this;
 			var reg = /^(\s+)/g, str = '';
 			if(reg.test(txt)){
 				str = RegExp.$1;
@@ -71,7 +72,6 @@
 			return txt;
 		},
 		_addHtmlColor: function(txt){
-			var _this = this;
 			txt = txt.replace(CC.regs.html.propval, function(){
 				return '=\"<span class="html_propval">' + arguments[1] + '</span>\"';
 			}).replace(CC.regs.html.prop, function(){
@@ -86,7 +86,6 @@
 			return txt;
 		},
 		_addCssColor: function(txt){
-			var _this = this;
 			txt = txt.replace(CC.regs.css.style, function(){
 				var str = arguments[0];
 				str = str.replace(CC.regs.css.tag, function(){
@@ -107,7 +106,6 @@
 			return txt;
 		},
 		_addJsColor: function(txt){
-			var _this = this;
 			txt = txt.replace(CC.regs.js.keyword, function(){
 				return '<span class="js_internal">' + arguments[1] + '</span>';
 			}).replace(CC.regs.js.number, function(){
@@ -116,6 +114,8 @@
 				return '<span class="js_note">' + arguments[0];
 			}).replace(CC.regs.js.noteend, function(){
 				return arguments[0] + '</span>';
+			}).replace(CC.regs.js.noteline, function(){
+				return '<span class="js_note">' + arguments[1] + '</span>';
 			});
 			return txt;
 		},
@@ -125,13 +125,13 @@
 				var format = _this.cs[i].getAttribute("format");
 				txt = _this.cs[i].innerHTML;
 				txt = _this._formart(txt);
-				if(format && format.indexOf("html") != -1){
+				if(format && format.indexOf("html") !== -1){
 					txt = _this._addHtmlColor(txt);
 				}
-				if(format && format.indexOf("css") != -1){
+				if(format && format.indexOf("css") !== -1){
 					txt = _this._addCssColor(txt);
 				}
-				if(format && format.indexOf("js") != -1){
+				if(format && format.indexOf("js") !== -1){
 					txt = _this._addJsColor(txt);
 				}
 				_this.cs[i].innerHTML = txt;
