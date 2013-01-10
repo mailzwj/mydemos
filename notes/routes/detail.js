ff2d327f680aa4c03ba311e202e96c2f5cd923c3
Mr.Zheng <mailzwj@126.com>
2013-01-10 23:19:00 +0800

    notes项目添加用户权限管理，新增node连接mysql案例

--- notes/routes/detail.js
+++ notes/routes/detail.js
@@ -9,85 +9,105 @@ var df = require("./dateformat").formatDate;
 var style = "yyyy-mm-dd hh:ii:ss";
 
 exports.main = function(req, res){
-	var coll = db.collection("posts");
-	//req.session.nowtime = new Date().getTime(); //Set Session
-	coll.find().sort({"posttime": -1}).limit(20).toArray(function(err, data){
-		if(err){
-			console.log("Err:" + err);
-		}
-		res.render('main', {
-			title: "NodeJS记事本",
-			docTitle: "最近记录的事件列表",
-			data: data
-		});
-	});
-};
-
-exports.show = function(req, res){
-	var id = req.params.id;
-	//var nt = req.session.nowtime; //Get Session
-	coll.update({_id: coll.id(id)}, {$inc:{"clicknum": 1}});
-	coll.findOne({_id: coll.id(id)}, function(err, data){
-		if(err){
-			console.log("Err:" + err);
-		}
-		res.render("detail", {
-			title: data.posttitle,
-			data: data
-		});
-	});
-};
-
-exports.add = function(req, res){
-	var _name = req.query.postname;
-	var _con = req.query.postcon;
-	if(_name){
-		coll.save({"posttitle": _name, "postcontent": _con, "posttime": df(new Date(), style), "uptime": df(new Date(), style), "clicknum": 0}, function(err){
+	if(req.session.username){
+		var coll = db.collection("posts");
+		//req.session.nowtime = new Date().getTime(); //Set Session
+		coll.find().sort({"posttime": -1}).limit(20).toArray(function(err, data){
 			if(err){
 				console.log("Err:" + err);
-				res.redirect("./add");
-			}else{
-				res.redirect("/");
 			}
+			res.render('main', {
+				title: "NodeJS记事本",
+				docTitle: "最近记录的事件列表",
+				data: data
+			});
 		});
 	}else{
-		res.render("add", {title: "新增记事"});
+		res.redirect("/");
 	}
 };
 
-exports.edit = function(req, res){
-	var id = req.params.id;
-	var _name = req.query.postname;
-	var _con = req.query.postcon;
-	if(_name){
-		coll.update({_id: coll.id(id)}, {$set:{"posttitle": _name, "postcontent": _con, "uptime": df(new Date(), style)}}, function(err){
-			if(err){
-				console.log("Err:" + err);
-				res.redirect("./edit/" + id);
-			}else{
-				res.redirect("/");
-			}
-		});
-	}else{
+exports.show = function(req, res){
+	if(req.session.username){
+		var id = req.params.id;
+		//var nt = req.session.nowtime; //Get Session
+		coll.update({_id: coll.id(id)}, {$inc:{"clicknum": 1}});
 		coll.findOne({_id: coll.id(id)}, function(err, data){
 			if(err){
 				console.log("Err:" + err);
 			}
-			res.render("edit", {
-				title: "编辑 ——> " + data.posttitle,
-				data: data,
-				postId: id
+			res.render("detail", {
+				title: data.posttitle,
+				data: data
 			});
 		});
+	}else{
+		res.redirect("/");
+	}
+};
+
+exports.add = function(req, res){
+	if(req.session.username){
+		var _name = req.query.postname;
+		var _con = req.query.postcon;
+		if(_name){
+			coll.save({"posttitle": _name, "postcontent": _con, "posttime": df(new Date(), style), "uptime": df(new Date(), style), "clicknum": 0}, function(err){
+				if(err){
+					console.log("Err:" + err);
+					res.redirect("./add");
+				}else{
+					res.redirect("/");
+				}
+			});
+		}else{
+			res.render("add", {title: "新增记事"});
+		}
+	}else{
+		res.redirect("/");
+	}
+};
+
+exports.edit = function(req, res){
+	if(req.session.username){
+		var id = req.params.id;
+		var _name = req.query.postname;
+		var _con = req.query.postcon;
+		if(_name){
+			coll.update({_id: coll.id(id)}, {$set:{"posttitle": _name, "postcontent": _con, "uptime": df(new Date(), style)}}, function(err){
+				if(err){
+					console.log("Err:" + err);
+					res.redirect("./edit/" + id);
+				}else{
+					res.redirect("/");
+				}
+			});
+		}else{
+			coll.findOne({_id: coll.id(id)}, function(err, data){
+				if(err){
+					console.log("Err:" + err);
+				}
+				res.render("edit", {
+					title: "编辑 ——> " + data.posttitle,
+					data: data,
+					postId: id
+				});
+			});
+		}
+	}else{
+		res.redirect("/");
 	}
 };
 
 exports.del = function(req, res){
-	var id = req.params.id;
-	coll.remove({_id: coll.id(id)}, function(err){
-		if(err){
-			console.log("Err:" + err);
-		}
+	if(req.session.username){
+		var id = req.params.id;
+		coll.remove({_id: coll.id(id)}, function(err){
+			if(err){
+				console.log("Err:" + err);
+			}
+			res.redirect("/");
+		});
+	}else{
 		res.redirect("/");
-	});
+	}
 };
--- notes/routes/index.js
+++ notes/routes/index.js
@@ -3,6 +3,26 @@
  * GET home page.
  */
 
+var db = require("../config").db;
+var coll = db.collection("user");
 exports.index = function(req, res){
-	res.render("index", {title: "登录我的记事本"});
+    if(!req.session.username){
+        var _user = req.query.username;
+        var _pwd = req.query.userpwd;
+        if(_pwd){
+            coll.findOne({username: _user, password: _pwd}, function(err, data){
+                if(err){
+                    console.log("Err:" + err);
+                }
+                if(data && data.username){
+                    req.session.username = data.username;
+                }
+                res.redirect("/");
+            });
+        }else{
+            res.render("index", {title: "登录我的记事本"});
+        }
+    }else{
+        res.redirect("/main");
+    }
 };
